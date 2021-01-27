dfe071243fec5863fdbab1494ceff0f005aab0b0
Aman Sharma <amansharma2910@gmail.com>
2021-01-27 22:50:17 +0530

    Completed project

--- setup.py
+++ setup.py
@@ -1,9 +1,11 @@
 from setuptools import setup
 
 setup(
-    name='Sortify',
-    version='0.1',
+    name='sortify',
+    version='1.0',
     description='A Python package containing implementation of various sorting algorithms.',
-    packages=['Sortify'],
+    packages=['sortify'],
+    author='Aman Sharma',
+    author_email='amansharma2910@gmail.com',
     zip_safe=False,
 )
